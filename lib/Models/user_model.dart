c7a1f68853d17a444eb908242d0e2d185418f7f0
apalex <alexpham2003@gmail.com>
2024-01-16 18:10:43 -0500

    Updated User_Info Model

--- lib/Models/user_model.dart
+++ lib/Models/user_model.dart
@@ -1,44 +1,48 @@
 class User {
-  final int id;
+  final int userId;
   final String firstName;
   final String lastName;
   final String username;
   final String email;
   final String phoneNum;
-  final String password;
+  final String userPassword;
   final String createdOn;
+  final int isActive;
 
   const User({
-    required this.id,
+    required this.userId,
     required this.firstName,
     required this.lastName,
     required this.username,
     required this.email,
     required this.phoneNum,
-    required this.password,
+    required this.userPassword,
     required this.createdOn,
+    required this.isActive
   });
 
-  factory User.fromJson(Map<String, dynamic> json) => User(
-    id: json['id'],
+  factory User.fromMap(Map<String, dynamic> json) => User(
+    userId: json['userId'],
     firstName: json['firstName'],
     lastName: json['lastName'],
     username: json['username'],
     email: json['email'],
     phoneNum: json['phoneNum'],
-    password: json['password'],
-    createdOn: json['createdOn']
+    userPassword: json['userPassword'],
+    createdOn: json['createdOn'],
+    isActive: json['isActive']
   );
   
-  Map<String, dynamic> toJson() => {
-    'id': id,
+  Map<String, dynamic> toMap() => {
+    'userId': userId,
     'firstName': firstName,
     'lastName': lastName,
     'username': username,
     'email': email,
     'phoneNum': phoneNum,
-    'password': password,
-    'createdOn': createdOn
+    'userPassword': userPassword,
+    'createdOn': createdOn,
+    'isActive': isActive
   };  
 
 }
