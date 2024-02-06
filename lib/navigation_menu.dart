1d7fff80bd8220c9ae2e939f2ee016bb596d9009
apalex <alexpham2003@gmail.com>
2024-02-06 17:35:45 -0500

    Reworked Navigation Menu

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -3,46 +3,76 @@ import 'package:crypto_app/markets.dart';
 import 'package:crypto_app/trade.dart';
 import 'package:crypto_app/futures.dart';
 import 'package:crypto_app/portfolio.dart';
+import 'package:crypto_app/Models/user_model.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class NavigationMenu extends StatelessWidget {
-  const NavigationMenu({Key? key}) : super(key: key);
+class NavigationMenu extends StatefulWidget {
+  final User? user;
+  const NavigationMenu({super.key, this.user});
+
+  @override
+  State<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  int currentIndex = 0;
+  late Home homeScreen = Home(user: widget.user,);
+  late final screens = [
+    homeScreen,
+    Markets(),
+    Trade(),
+    Futures(),
+    Portfolio()
+  ];
+
+  @override
+  void initState() {
+    homeScreen = Home(user: widget.user,);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(NavigationController());
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Obx(() => controller.screens[controller.selectedIndex.value]),
-        bottomNavigationBar: Obx(
-          () => NavigationBar(
-            height: 65,
-            elevation: 1,
-            selectedIndex: controller.selectedIndex.value,
-            onDestinationSelected: (index) => controller.selectedIndex.value = index,
-            destinations: const [
-              NavigationDestination(icon: Icon(Icons.home_sharp), label: "Home"),
-              NavigationDestination(icon: Icon(Icons.bar_chart_sharp), label: "Markets"),
-              NavigationDestination(icon: Icon(Icons.compare_arrows_sharp), label: "Trade"),
-              NavigationDestination(icon: Icon(Icons.keyboard_double_arrow_up_sharp), label: "Futures"),
-              NavigationDestination(icon: Icon(Icons.wallet_rounded), label: "Portfolio"),
-            ],
-          ),
+        body: screens[currentIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: currentIndex,
+          height: 65,
+          elevation: 1,
+          onDestinationSelected: (index) => setState(() {
+            currentIndex = index;
+          }),
+          destinations: const [
+            // Home
+            NavigationDestination(
+              icon: Icon(Icons.home_sharp),
+              label: "Home"
+            ),
+            // Markets
+            NavigationDestination(
+              icon: Icon(Icons.bar_chart_sharp),
+              label: "Markets"
+            ),
+            // Trade
+            NavigationDestination(
+              icon: Icon(Icons.compare_arrows_sharp),
+              label: "Trade"
+            ),
+            // Futures
+            NavigationDestination(
+              icon: Icon(Icons.keyboard_double_arrow_up_sharp),
+              label: "Futures"
+            ),
+            // Portfolio
+            NavigationDestination(
+              icon: Icon(Icons.wallet_rounded),
+              label: "Portfolio"
+            ),
+          ],
         ),
       ),
-  );
+    );
   }
-}
-
-class NavigationController extends GetxController {
-  final Rx<int> selectedIndex = 0.obs;
-  final screens = [
-    const Home(),
-    const Markets(),
-    Trade(),
-    const Futures(),
-    const Portfolio(),
-  ];
 }
